0bb57ce66cd1274625cd578eb095656e8ab1291e
Mihir Nayak <99991521+mihir2226@users.noreply.github.com>
2023-07-26 12:06:02 +0530

    Add files via upload

---
+++ odoo wizard/print_all_property.py
@@ -0,0 +1,19 @@
+from odoo import fields,models
+
+class PrintAllPropertyWizard(models.TransientModel):
+	_name = "print.all.property.wizard"
+	_description = "prints all property"
+
+	old_partner_id = fields.Many2one('res.users', string="Old Partner")
+	new_partner_id = fields.Many2one('res.users', string="New Partner")
+
+
+	def add_info(self):
+		# print("\n\n\n\n\n\n -----------", self.env.context)
+		# property_id = self.env['estate.property'].browse(self.env.context.get('active_id'))
+		property_id = self.env['estate.property'].search([('salesman','=',self.old_partner_id.name)])
+		for rec in property_id:
+			rec.salesman = self.new_partner_id
+
+	#selling price admin ne dekhavi joia
+	#menuitem bas admin ne dekhavi joia
